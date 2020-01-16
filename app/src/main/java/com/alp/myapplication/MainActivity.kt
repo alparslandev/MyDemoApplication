101ae5619818f6231cf5bb6efad67d05ff09ab29
AlparslanDev <alparslan.develioglu@peakup.org>
2020-01-16 17:04:09 +0300

    Initial commit

---
+++ app/src/main/java/com/alp/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alp.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
